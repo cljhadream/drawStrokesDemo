6d35dbff5449b74fa350c04076a0a5bd9a7166e0
cljhadream <cljhadream@163.com>
2026-01-13 10:40:23 +0800

    feat:git删除无效文件

--- app/build5/intermediates/cxx/Debug/3s4f185k/logs/armeabi-v7a/build_command_native-lib.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\sunia\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Documents\\Myproject\\Android\\HelloWorld\\app\\.cxx\\Debug\\3s4f185k\\armeabi-v7a" ^
-  native-lib
--- app/build5/intermediates/cxx/Debug/3s4f185k/logs/x86/build_command_native-lib.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\sunia\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Documents\\Myproject\\Android\\HelloWorld\\app\\.cxx\\Debug\\3s4f185k\\x86" ^
-  native-lib
